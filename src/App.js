2f92b88e9ce031b06970756df994e903922de971
Ekta Varu <varuekta03@gmail.com>
2023-08-06 20:01:13 +0530

    Creating Navbar in newsapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
       </div>
     )
   }
