2bd6383be568d6f94efe128d6afb5c148c1d1166
Ekta Varu <varuekta03@gmail.com>
2023-08-10 19:17:40 +0530

    Adding cards and grids using bootstrap

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-            This is News Component
-            <NewsItem />
-            <NewsItem />
-            <NewsItem />
-            <NewsItem />
+      <div className='container my-3'>
+            <h2 className='my-4'>NewsApp - Top Headlines :</h2>
+            <div className="row">
+                <div className="col-md-4">
+                  <NewsItem title="myTitle" description="mydesc" />
+                </div>
+                <div className="col-md-4">
+                  <NewsItem title="myTitle" description="mydesc" />
+                </div>
+                <div className="col-md-4">
+                  <NewsItem title="myTitle" description="mydesc" />
+                </div>
+            </div>
       </div>
     )
   }
