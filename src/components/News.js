19044c3fe678a123d858a1252622c077ad0c7038
Ekta Varu <varuekta03@gmail.com>
2023-08-10 20:21:31 +0530

    Adding newsItems using states and newsapi

--- src/components/News.js
+++ src/components/News.js
@@ -36,15 +36,13 @@ export class News extends Component {
       <div className='container my-3'>
             <h2 className='my-4'>NewsApp - Top Headlines :</h2>
             <div className="row">
-                <div className="col-md-4">
-                  <NewsItem title="myTitle" description="mydesc" />
+            {this.state.articles.map((element)=>{
+              return(
+                <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
                 </div>
-                <div className="col-md-4">
-                  <NewsItem title="myTitle" description="mydesc" />
-                </div>
-                <div className="col-md-4">
-                  <NewsItem title="myTitle" description="mydesc" />
-                </div>
+              );
+            })}
             </div>
       </div>
     )
