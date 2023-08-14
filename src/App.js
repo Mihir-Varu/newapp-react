c6ba20bea3c7a692a5681b13d7c3a3d343acdc47
Ekta Varu <varuekta03@gamil.com>
2023-08-14 20:25:11 +0530

    extra removed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>hello</h1>
-        <h2>adding some new lines</h2>
         <NavBar />
         <News />
       </div>
