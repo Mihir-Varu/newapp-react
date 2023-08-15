56bbe29eb3c5ea7c0078d6fa39667f60b1454db6
Ekta Varu <varuekta03@gmail.com>
2023-08-15 19:42:35 +0530

    removing slicing and static articles

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,10 +6,10 @@ export class NewsItem extends Component {
     return (
       <div>
           <div className="card" style={{width:  "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img src={!imageUrl?"https://c.ndtvimg.com/2022-08/4c85ndmg_independence-day-2022-hoisting-national-flag-at-home_625x300_08_August_22.jpg":imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
-              <p className="card-text">{description}...</p>
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
               <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
             </div>
           </div>
