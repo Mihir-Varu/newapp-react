a41a52431e6c64dcefc44f84481c082443d6bf0d
Ekta Varu <varuekta03@gmail.com>
2023-08-22 23:11:10 +0530

    adding extra news categories

--- src/App.js
+++ src/App.js
@@ -10,7 +10,35 @@ export default class App extends Component {
     return (
       <div>
         <NavBar />
-        <News pageSize={5} />
+        {/* <Router>
+          <NavBar />
+          <Routes>
+              <Route path='/'>
+                  <News pageSize={5} country="in" category="general" />
+              </Route>
+              <Route path='/business'>
+                  <News pageSize={5} country="in" category="business" />
+              </Route>
+              <Route path='/entertainment'>
+                  <News pageSize={5} country="in" category="entertainment" />
+              </Route>
+              <Route path='/general'>
+                  <News pageSize={5} country="in" category="general" />
+              </Route>
+              <Route path='/health'>
+                  <News pageSize={5} country="in" category="health" />
+              </Route>
+              <Route path='/science'>
+                  <News pageSize={5} country="in" category="science" />
+              </Route>
+              <Route path='/sports'>
+                  <News pageSize={5} country="in" category="sports" />
+              </Route>
+              <Route path='/technology'>
+                  <News pageSize={5} country="in" category="technolo" />
+              </Route>
+          </Routes>
+        </Router> */}
       </div>
     )
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,25 @@ export class NavBar extends Component {
                     <a className="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/about">About</a>
+                    <a className="nav-link" href="/business">Business</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/entertainment">Entertainment</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/general">General</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/health">Health</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/science">Science</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/sports">Sports</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/technology">Technology</a>
                     </li>
                 </ul>
                 </div>
